ec1e4d85c3f25c37d879730b784473c6df9aafef
Xi LIU <xi.liu@u.nus.edu>
2023-12-04 20:50:25 +0800

    sanity check for supercell

---
+++ OOPmain.py
@@ -0,0 +1,207 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pathlib
+from datetime import datetime
+import sympy as smp
+
+plt.close("all")
+# lattice class
+from lattice.baseLattice import lattice
+
+# Main
+# from cd.SymGroup import GetSpaceGroupPrimitive
+from cd.SymGroup import paraInPrim2Conv,conv2SGN,getParaSym
+from cd.NbrAtom  import FindNeighbor
+from cd.SymAtom  import FindAtomSymmetry
+from cd.HopRel   import FindRelation
+from cd.HopVal   import GetHoppingValue
+from cd.HmtReal  import GetHamiltonianReal
+from cd.KPoint   import GetKPoint
+from cd.HmtK     import GetHamiltonianK
+from cd.HmtK     import HkSp2Np
+from cd.Eig      import GetEigenSolution
+from cd.Berry    import GetBerryK
+from cd.Berry    import GetAHNC
+from cd.Dos      import GetDos
+from cd.Strip    import GetStrip
+from cd.EigStrip import GetEigStrip
+from cd.Qc       import GetQc
+# Read & Write
+from rw.ReadTBIN import ReadInput
+from rw.ReadHop  import ReadHopping
+from rw.ReadRel  import ReadRelation
+from rw.ReadHmt  import ReadHamiltonianReal
+from rw.ReadKpt  import ReadKPoint
+from rw.WriteRel import WriteRelation
+from rw.WriteHmt import WriteHamiltonianReal
+from rw.WriteAna import WriteHamiltonianAnalytic
+from rw.WriteAna import writeHkAnalytic
+from rw.printHk  import Hk2html
+from rw.ReadBer  import ReadBerryPara
+from rw.ReadDos  import ReadDosPara
+from rw.ReadQc   import ReadQcPara
+# Plot
+from pl.PlotEB   import PlotEnergyBand
+from pl.PlotEB   import plotEigsFromHkSp
+from pl.PlotBC   import PlotBerryCurvature
+from pl.PlotDos  import PlotDOS
+from pl.PlotStrip import PlotAtomStrip
+from pl.PlotEB1D import PlotEnergyBand1D
+from pl.PlotQc   import PlotQuanCond
+# from pl.PlotAt   import PlotAtoms
+# from pl.PlotHop  import PlotHoppingTerm
+# from pl.PlotBZ  import PlotBrillouinZone
+# Check
+from ck.CheckOrbCpl    import CheckOrbitalCompleteness as CheckOrbital
+from ck.CheckHmtSym    import CheckHamiltonianSymmetry as CheckH
+from ck.CheckEigValSym import CheckEnergySymmetry as CheckE
+
+#This is the demo program for Auh-17-2023
+#1. It reads config file containing information of the crystal,
+# the reader may give either a primitive cell or a conventional cell,
+# the program will compute primitive from conventional, or conventional from primitive.
+#2. When the
+
+material = 'data/ABO3/super_TBIN_ABO3.txt'
+# material = 'data/Graphene/primitive_TBIN_Graphene.txt'
+# material = 'data/h-BN/primitive_TBIN_h-BN.txt'
+# material = 'data/NaCl/primitive_TBIN_NaCl.txt'
+# material = 'data/Si/primitive_TBIN_Si.txt'
+lenParams=len(sys.argv)
+if lenParams<2:
+    sys.argv.append(material)
+else:
+    sys.argv[1] = material
+
+lenParams=len(sys.argv)
+if lenParams!=2:
+    raise RuntimeError("Wrong number of arguments.")
+
+inConfigName=sys.argv[1]
+#check if given path exist
+if not os.path.exists(inConfigName):
+    raise NotADirectoryError("path does not exist.")
+#check if given file exists
+if not os.path.isfile(inConfigName):
+    raise FileNotFoundError("Not a file.")
+# print(inConfigName)
+# inConfigFolder=pathlib.Path(inConfigName).parent
+# print(inConfigFolder)
+#read info
+baseCrystal=lattice()
+baseCrystal.ParaIn=ReadInput(inConfigName)
+baseCrystal.checkSupercellInfoSanity()
+# Name=ParaIn["Name"]
+
+
+'''################### Determine space group of crystal ####################'''
+# ParaSym = GetSpaceGroupPrimitive(ParaIn)
+atmUnderConvVector,atmIndsConv=paraInPrim2Conv(baseCrystal.ParaIn)
+SGN, originBilbao=conv2SGN(baseCrystal.ParaIn,atmUnderConvVector,atmIndsConv)
+baseCrystal.ParaSym=getParaSym(baseCrystal.ParaIn,SGN, originBilbao)
+baseCrystal.ParaIn["origin Bilbao"]=baseCrystal.ParaSym["origin Bilbao"]
+
+
+'''##################### Complete Electronic orbitals ######################'''
+CheckOrbital(baseCrystal.ParaIn,baseCrystal.ParaSym)
+
+
+'''################### Relate hopping terms by symmetry ####################'''
+baseCrystal.ParaNbr    = FindNeighbor(baseCrystal.ParaIn)
+# PlotAtoms(baseCrystal.ParaIn,baseCrystal.ParaNbr,baseCrystal.ParaIn["Name"])
+tFindingRelationStart=datetime.now()
+baseCrystal.ParaSymAt  = FindAtomSymmetry(baseCrystal.ParaIn,baseCrystal.ParaSym,baseCrystal.ParaNbr)
+baseCrystal.ParaRel    = FindRelation(baseCrystal.ParaIn,baseCrystal.ParaSym,baseCrystal.ParaNbr,baseCrystal.ParaSymAt)
+# PlotHoppingTerm(baseCrystal.ParaIn,baseCrystal.ParaNbr,baseCrystal.ParaRel,baseCrystal.ParaIn["Name"],[5,6])
+tFindingRelationEnd=datetime.now()
+print("Finding symmetry relations: ",tFindingRelationEnd-tFindingRelationStart)
+WriteRelation(baseCrystal.ParaIn,baseCrystal.ParaRel)
+
+
+'''################### Calculate real space Hamiltonian ####################'''
+# From hopping relations to Hamiltonian in real space
+HopValIn   = ReadHopping(baseCrystal.ParaIn["Folder"]+"/HopValIN_"+baseCrystal.ParaIn["Name"]+".txt")
+# ParaRel    = ReadRelation(baseCrystal.ParaIn["Folder"]+ "/HopRel.txt")
+baseCrystal.HopValClas = GetHoppingValue(HopValIn, baseCrystal.ParaRel)
+ParaHmtR   = GetHamiltonianReal(baseCrystal.ParaRel,baseCrystal.HopValClas)
+WriteHamiltonianReal(baseCrystal.ParaIn, ParaHmtR)
+
+
+# '''###### Compute analytic forms of real space and k-space Hamiltonian #####'''
+# # Write analytic form of Hamiltonian
+# ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+# ParaKptIn  = ReadKPoint(baseCrystal.ParaIn["Folder"]+"/KptIN_" + baseCrystal.ParaIn["Name"] + ".txt")
+# ParaKpt    = GetKPoint(ParaKptIn)
+# WriteHamiltonianAnalytic(baseCrystal.ParaIn,baseCrystal.ParaRel)#Hamiltonian in real space
+# # Calculate energy bands from analytic form
+# HkMatSp,k1,k2,k3,tValsSpAll=writeHkAnalytic(baseCrystal.ParaIn,baseCrystal.ParaRel,ParaHmtR)# H(k): Hamiltonian in k-space
+# Hk2html(baseCrystal.ParaIn,HkMatSp,baseCrystal.ParaIn["Folder"])#write H(k) to html file
+# pltSpEigs=plotEigsFromHkSp(HkMatSp,k1,k2,k3,tValsSpAll,ParaKpt,HopValIn,baseCrystal.ParaRel,baseCrystal.ParaIn["Folder"])
+# HkTmp=HkSp2Np(HkMatSp,k1,k2,k3,tValsSpAll,1,2,3,HopValIn)
+
+
+'''######################## Calculate Energy Bands #########################'''
+# From Hamiltonian in real space to Hamiltonian in k space
+ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+ParaKptIn  = ReadKPoint(baseCrystal.ParaIn["Folder"]+"/KptIN_" + baseCrystal.ParaIn["Name"]+ ".txt")
+ParaKpt    = GetKPoint(ParaKptIn)
+ParaHmtK   = GetHamiltonianK(ParaHmtR,ParaKpt)
+# Solve the k-space Hamiltonian to get energy bands
+tEigStart=datetime.now()
+ParaEig    = GetEigenSolution(ParaHmtK)
+ParaEigPlt = PlotEnergyBand(ParaKpt,ParaEig,baseCrystal.ParaIn["Folder"])
+tEigEnd=datetime.now()
+print(baseCrystal.ParaIn["Name"]+ " enerygy band: ",tEigEnd-tEigStart)
+
+
+'''######### Check symmetry of Energy Bands and k-space Hamiltonian ########'''
+CheckE(baseCrystal.ParaIn,baseCrystal.ParaSym,baseCrystal.ParaSymAt,ParaHmtR,0)
+CheckH(baseCrystal.ParaIn,baseCrystal.ParaSym,baseCrystal.ParaSymAt,ParaHmtR)
+
+
+# '''####################### Calculate Berry Curvature #######################'''
+# # Read real space Hamiltonian and k points
+# ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+# ParaKptIn  = ReadKPoint(baseCrystal.ParaIn["Folder"]+"/KptIN_" + baseCrystal.ParaIn["Name"] + ".txt")
+# ParaKpt    = GetKPoint(ParaKptIn)
+# # Calculate Berry Curvature
+# ParaBerK = GetBerryK(baseCrystal.ParaIn,ParaHmtR,ParaKpt)
+# ParaBerPlt = PlotBerryCurvature(ParaKpt,ParaBerK,baseCrystal.ParaIn["Folder"])
+# '''This part needs an isolated test!'''
+
+
+# '''########### Calculate intrinsic anomalous Hall conductivity and 
+#                          intrinsic anomalous Nernst conductivity ###########'''
+# # Read real space Hamiltonian and input parameters (Temperature, Fermi Energy, nk)
+# ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+# ParaBerIn  = ReadBerryPara(baseCrystal.ParaIn["Folder"]+ "/BerIN_" + baseCrystal.ParaIn["Name"] + ".txt")
+# # Calculate intrinsic AHC & ANC
+# ParaAHNC = GetAHNC(baseCrystal.ParaIn,ParaHmtR,ParaBerIn)
+# '''This part needs an isolated test!'''
+
+
+# '''###################### Calculate density of states ######################'''
+# # Read real space Hamiltonian and input parameters (method,nE,nk)
+# ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+# ParaDosIn  = ReadDosPara(baseCrystal.ParaIn["Folder"]+ "/DosIN_" + baseCrystal.ParaIn["Name"] + ".txt")
+# # Calculate DOS
+# ParaDos = GetDos(baseCrystal.ParaIn,ParaHmtR,ParaDosIn)
+# ParaDosPlt = PlotDOS(ParaDos,baseCrystal.ParaIn["Folder"])
+
+'''######################## Calculate info of strip ########################'''
+# Read real space Hamiltonian and input parameters (method,nE,nk)
+ParaHmtR   = ReadHamiltonianReal(baseCrystal.ParaIn)
+ParaQcIn   = ReadQcPara(baseCrystal.ParaIn["Folder"]+ "/QcIN_" + baseCrystal.ParaIn["Name"] + ".txt")
+# Info of strip
+ParaStrip  = GetStrip(baseCrystal.ParaIn,ParaHmtR,ParaQcIn)
+# Plot strip
+PlotAtomStrip(ParaStrip,baseCrystal.ParaIn["Name"])
+# Calculate energy band
+ParaEigStrip = GetEigStrip(ParaStrip)
+PlotEnergyBand1D(ParaEigStrip,baseCrystal.ParaIn["Folder"])
+# Calculate quantum conductance
+ParaQc     = GetQc(ParaQcIn,ParaStrip)
+PlotQuanCond(ParaQc,baseCrystal.ParaIn["Folder"])
+'''This part is only tested with graphene & BN!'''
--- lattice/baseLattice.py
+++ lattice/baseLattice.py
@@ -1,41 +1,105 @@
-import numpy as np
-from lattice.assembleMixinModules import mixinModules
+# import numpy as np
 
 
 
 
-class lattice(*mixinModules):
+
+class lattice:
     #base class for a lattice
     def __init__(self):
-        self.keywords=["Name", "Dim", "Spin", "Nbr",
-          "LatType", "LatVec", "AtTpNum","Bases",
-          "AtomSite", "supercellSize","supercellVacancy","supercellSubstitution",
-          "supercellInterstitial"]
+        self.ParaIn={}
+        self.ParaSym={}
+        self.ParaNbr={}
+        self.ParaSymAt={}
+        self.ParaRel={}
+        self.HopValClas=[]
 
-        self.OrbGrp = np.array(["1S","2S","2P","2P","2P",
-                   "3S","3P","3P","3P","3D","3D","3D","3D","3D",
-                   "4S","4P","4P","4P","4D","4D","4D","4D","4D",
-                   "4F","4F","4F","4F","4F","4F","4F",
-                   "5S","5P","5P","5P","5D","5D","5D","5D","5D",
-                   "5F","5F","5F","5F","5F","5F","5F",
-                   "6S","6P","6P","6P","6D","6D","6D","6D","6D",
-                   "6F","6F","6F","6F","6F","6F","6F",
-                   "7S","7P","7P","7P","7D","7D","7D","7D","7D",
-                   "7F","7F","7F","7F","7F","7F","7F",
-                   "S","P","P","P","D","D","D","D","D",
-                   "F","F","F","F","F","F","F"])
-        self.OrbIdv = np.array(["1s","2s","2px","2py","2pz",
-                   "3s","3px","3py","3pz","3dxy","3dyz","3dzx","3dx2-y2","3dz2",
-                   "4s","4px","4py","4pz","4dxy","4dyz","4dzx","4dx2-y2","4dz2",
-                   "4fz3","4fxz2","4fyz2","4fxyz","4fz(x2-y2)","4fx(x2-3y2)","4fy(3x2-y2)",
-                   "5s","5px","5py","5pz","5dxy","5dyz","5dzx","5dx2-y2","5dz2",
-                   "5fz3","5fxz2","5fyz2","5fxyz","5fz(x2-y2)","5fx(x2-3y2)","5fy(3x2-y2)",
-                   "6s","6px","6py","6pz","6dxy","6dyz","6dzx","6dx2-y2","6dz2",
-                   "6fz3","6fxz2","6fyz2","6fxyz","6fz(x2-y2)","6fx(x2-3y2)","6fy(3x2-y2)",
-                   "7s","7px","7py","7pz","7dxy","7dyz","7dzx","7dx2-y2","7dz2",
-                   "7fz3","7fxz2","7fyz2","7fxyz","7fz(x2-y2)","7fx(x2-3y2)","7fy(3x2-y2)",
-                   "s","px","py","pz","dxy","dyz","dzx","dx2-y2","dz2",
-                   "fz3","fxz2","fyz2","fxyz","fz(x2-y2)","fx(x2-3y2)","fy(3x2-y2)"])
+    def atomName2Num(self,atm):
+        """
+
+        :param atm: atom name
+        :return: number of atoms
+        """
+        ind= self.ParaIn["AtomName"].index(atm)
+        return self.ParaIn["AtomNumber"][ind]
+    def checkSupercellInfoSanity(self):
+        if len(self.ParaIn["supercell"])==0:
+            return
+
+        if not "supercellSize" in self.ParaIn["supercell"]:
+            raise ValueError("Supercell size not specified.")
+        if (not "supercellVacancy" in self.ParaIn["supercell"])\
+            and (not "supercellSubstitution" in self.ParaIn["supercell"])\
+            and (not "supercellInterstitial" in self.ParaIn["supercell"]):
+            raise ValueError("At least give one of the values: supercellVacancy, supercellSubstitution, supercellInterstitial.")
+        # check if supercellSize are positive integers
+        # supercellSize is already int by the means of reading
+        for num in self.ParaIn["supercell"]["supercellSize"]:
+            if num<=0:
+                raise ValueError("supercell size should be >0.")
+
+        #check if supercellVacancy is within the range of supercellSize
+        if "supercellVacancy" in self.ParaIn["supercell"]:
+            for row in self.ParaIn["supercell"]["supercellVacancy"]:
+                n0n1n2,j,atm=row
+                n0,n1,n2=n0n1n2
+                #n0,n1,n2 is the base cell with the vacancy
+                if (n0<0 or n0>=self.ParaIn["supercell"]["supercellSize"][0])\
+                    or (n1<0 or n1 >=self.ParaIn["supercell"]["supercellSize"][1])\
+                    or (n2<0 or n2 >=self.ParaIn["supercell"]["supercellSize"][2]):
+                    raise ValueError("invalid vacancy position.")
+                numOfAtm=self.atomName2Num(atm)
+                #j-th atom to be replaced by vacancy, among all the same kinds of atoms
+                if j<0:
+                    raise ValueError("Value of vacancy should be positive.")
+                if j>=numOfAtm:
+                    raise ValueError("Value of vacancy of atom "+atm+" should be smaller than "+str(numOfAtm)+".")
 
 
 
+        #check if supercellSubstitution is valid
+        if "supercellSubstitution" in self.ParaIn["supercell"]:
+            for row in self.ParaIn["supercell"]["supercellSubstitution"]:
+                n0n1n2,j,atmOld,atmNewAndOrbs=row
+                n0,n1,n2=n0n1n2
+                atmNew=atmNewAndOrbs[0]
+                orbsNew=atmNewAndOrbs[1:]
+                if len(orbsNew)==0:
+                    raise ValueError("Please give the orbitals of the new atom.")
+
+                # n0,n1,n2 is the base cell with the vacancy
+                if (n0 < 0 or n0 >= self.ParaIn["supercell"]["supercellSize"][0]) \
+                        or (n1 < 0 or n1 >= self.ParaIn["supercell"]["supercellSize"][1]) \
+                        or (n2 < 0 or n2 >= self.ParaIn["supercell"]["supercellSize"][2]):
+                    raise ValueError("invalid substitution position.")
+                atmOldNum=self.atomName2Num(atmOld)#will raise exception if atmOld is not valid
+                if j<0:
+                    raise ValueError("The 3rd argument in supercellSubstitution is negative.")
+                if j>= atmOldNum:
+                    raise ValueError("The 3rd argument in supercellSubstitution should be smaller than "+str(atmOldNum)+".")
+
+
+
+        #check if supercellInterstitial is valid
+        if "supercellInterstitial" in self.ParaIn["supercell"]:
+            for row in self.ParaIn["supercell"]["supercellInterstitial"]:
+                n0n1n2,s0s1s2,atm,orbs=row
+                n0,n1,n2=n0n1n2
+                s0,s1,s2=s0s1s2
+                # n0,n1,n2 is the base cell with the vacancy
+                if (n0 < 0 or n0 >= self.ParaIn["supercell"]["supercellSize"][0]) \
+                        or (n1 < 0 or n1 >= self.ParaIn["supercell"]["supercellSize"][1]) \
+                        or (n2 < 0 or n2 >= self.ParaIn["supercell"]["supercellSize"][2]):
+                    raise ValueError("invalid interstitial position.")
+                #check if fractional coordinates are valid:
+                if s0<0 or s0>1:
+                    raise ValueError("Value of s0 is not valid.")
+                if s1<0 or s1>1:
+                    raise ValueError("Value of s1 is not valid.")
+                if s2<0 or s2>1:
+                    raise ValueError("Value of s2 is not valid.")
+                if len(orbs)==0:
+                    raise ValueError("Please give the orbitals of interstitial atom "+atm+".")
+
+
+        return
