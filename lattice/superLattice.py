d592914bb9d278cd525c57b69c135513fb7914dd
Xi LIU <xi.liu@u.nus.edu>
2023-12-08 13:57:49 +0800

    construct superlattice

--- lattice/superLattice.py
+++ lattice/superLattice.py
@@ -1,6 +1,6 @@
 from lattice.baseLattice import baseLattice
 import numpy as np
-
+import copy
 class superLattice(baseLattice):
     # class for a superlattice
     def __init__(self):
@@ -40,15 +40,67 @@ class superLattice(baseLattice):
         #
 
         #TODO: for now we assume that when the interstitial atoms are the same as in the baseLattice, they also have the same orbitals
-        #TODO: for now we make the restriction that the substuted atom is different from the new atom
+        #TODO: for now we make the restriction that the substituted atom is different from the new atom
         #duplicate the baseLattice's atoms Nj times along direction j, j=0,1,2
 
         #construct set cellRowAtm
         cellRowAtm=set()
-        # for n0 in range(0,N0):
-        #     for n1 in range(0,N1):
-        #         for n2 in range(0,N2):
+
+        for n0 in range(0,N0):
+            for n1 in range(0,N1):
+                for n2 in range(0,N2):
+                    for j,ind in enumerate(self.ParaIn["AtomTypeIndex"]):
+                        atm=self.ParaIn["AtomName"][ind]
+                        cellRowAtm.add((n0,n1,n2,j,atm))
+        # delete elements from cellRowAtm according to supercellVacancy
+        for row in self.ParaIn["supercell"]["supercellVacancy"]:
+            n0n1n2, k, atm = row
+            n0, n1, n2 = n0n1n2
+            toDelete=(n0,n1,n2,self.map2RowNum(k,atm),atm)
+            cellRowAtm.remove(toDelete)
+        # construct superLattice from supercellSubstitution
+        for row in self.ParaIn["supercell"]["supercellSubstitution"]:
+            n0n1n2, k, atmOld, atmNewAndOrbs = row
+            n0, n1, n2 = n0n1n2
+            atmNew = atmNewAndOrbs[0]
+            j=self.map2RowNum(k,atmOld)
+            toDelete=(n0,n1,n2,j,atmOld)
+            cellRowAtm.remove(toDelete)
+            toAdd=(n0,n1,n2,j,atmNew)
+            cellRowAtm.add(toAdd)
+
+        # construct supercellLattice from supercellInterstitial
+        self.superLatticeParaIn["AtomSite"]=copy.deepcopy(self.ParaIn["AtomSite"])
+        for row in self.ParaIn["supercell"]["supercellInterstitial"]:
+            n0n1n2, s0s1s2, atm, orbs = row
+            n0, n1, n2 = n0n1n2
+            s0, s1, s2 = s0s1s2
+            self.superLatticeParaIn["AtomSite"]=np.vstack([self.superLatticeParaIn["AtomSite"],[s0,s1,s2]])
+            l=len(self.superLatticeParaIn["AtomSite"])
+            toAdd=(n0,n1,n2,l-1,atm)
+            cellRowAtm.add(toAdd)
+
+
+
+
 
 
 
         return
+
+    def map2RowNum(self,k,atm):
+        """
+
+        :param k: ordinal of atm among the same atms in a baseLattice
+        :param atm: atom name
+        :return: row of atm in self.ParaIn["AtomSite"], in order to construct superLattice's parameters from
+        supercellVacancy and supercellSubstitution
+        """
+
+        #index of atm  in all atom names
+        ind=self.ParaIn["AtomName"].index(atm)
+
+        #posistions of ind in self.ParaIn["AtomTypeIndex"], which is also the row number of atm in self.ParaIn["AtomSite"]
+        positions=[i for i,val in enumerate(self.ParaIn["AtomTypeIndex"]) if i==ind]
+        return positions[k]
+
